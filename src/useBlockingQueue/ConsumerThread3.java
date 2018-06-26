e170049a83abf8c2bfed4515c3c0d8c9f36a6bf0
Ezreal <1649811141@qq.com>
2018-06-26 23:50:18 +0800

    使用阻塞队列实现

---
+++ src/useBlockingQueue/ConsumerThread3.java
@@ -0,0 +1,21 @@
+package useBlockingQueue;
+
+class ConsumerThread3 extends Thread {
+	private Resource3 resource3;
+  
+	public ConsumerThread3(Resource3 resource) {
+		this.resource3 = resource;
+		//setName("Ïû·ÑÕß");
+	}
+  
+	public void run() {
+		while (true) {
+			try {
+				Thread.sleep((long) (1000 * Math.random()));
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			resource3.remove();
+		}
+	}
+}
---
+++ src/useBlockingQueue/ProducerThread3.java
@@ -0,0 +1,20 @@
+package useBlockingQueue;
+
+class ProducerThread3 extends Thread{
+	private Resource3 resource3;
+	public ProducerThread3(Resource3 resource) {
+		this.resource3 = resource;
+        //setName("ЩњВњеп");
+    }
+
+    public void run() {
+    	while (true) {
+    		try {
+    			Thread.sleep((long) (1000 * Math.random()));
+    		} catch (InterruptedException e) {
+    			e.printStackTrace();
+    		}
+    		resource3.add();
+    	}
+    }
+}
